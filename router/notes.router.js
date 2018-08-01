61521db817c90936fd49463c801d890b9bd173c7
Charutha Bandara <charuthabandara@gmail.com>
2018-08-01 17:23:50 -0400

    final changes

--- router/notes.router.js
+++ router/notes.router.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const router = express.Router();
+
 // // Simple In-Memory Database
 const data = require('../db/notes');
 const simDB = require('../db/simDB');
